3854db1048af97fb1b53729fe10dc42981d755a6
Ceri <ceriwoolway@gmail.com>
2024-11-29 16:11:32 +0000

    Add loops to test for improved robustness

--- src/logic/placeShips.test.ts
+++ src/logic/placeShips.test.ts
@@ -2,31 +2,39 @@ import { generateRandomPosition } from './placeShips';
 
 describe('generateRandomShipPosition', () => {
   test('should ensure the startingColumn leaves enough space for the ship', () => {
-    const { startingColumn } = generateRandomPosition({ name: 'cruiser', size: 4 });
-    expect(startingColumn).toBeLessThanOrEqual(5);
+    for (let i = 0; i < 100; i++) {
+      const { startingColumn } = generateRandomPosition({ name: 'cruiser', size: 4 });
+      expect(startingColumn).toBeLessThanOrEqual(6);
+    }
   });
 
   test('should return a row and startingColumn within the board bounds', () => {
-    const { row, startingColumn } = generateRandomPosition({ name: 'cruiser', size: 3 });
+    for (let i = 0; i < 100; i++) {
+      const { row, startingColumn } = generateRandomPosition({ name: 'cruiser', size: 3 });
 
-    expect(row).toBeGreaterThanOrEqual(0);
-    expect(row).toBeLessThan(10);
-    expect(startingColumn).toBeGreaterThanOrEqual(0);
-    expect(startingColumn).toBeLessThanOrEqual(9);
+      expect(row).toBeGreaterThanOrEqual(0);
+      expect(row).toBeLessThan(10);
+      expect(startingColumn).toBeGreaterThanOrEqual(0);
+      expect(startingColumn).toBeLessThanOrEqual(9);
+    }
   });
 
   test('should handle ships of size 1 correctly', () => {
-    const { row, startingColumn } = generateRandomPosition({ name: 'cruiser', size: 1 });
+    for (let i = 0; i < 100; i++) {
+      const { row, startingColumn } = generateRandomPosition({ name: 'cruiser', size: 1 });
 
-    expect(row).toBeGreaterThanOrEqual(0);
-    expect(row).toBeLessThan(10);
-    expect(startingColumn).toBeGreaterThanOrEqual(0);
-    expect(startingColumn).toBeLessThan(10);
+      expect(row).toBeGreaterThanOrEqual(0);
+      expect(row).toBeLessThan(10);
+      expect(startingColumn).toBeGreaterThanOrEqual(0);
+      expect(startingColumn).toBeLessThan(10);
+    }
   });
 
   test('a theoretical size 10 ship should only be able to start in column 0', () => {
-    const shipPosition = generateRandomPosition({ name: 'cruiser', size: 10 });
-    expect(shipPosition.startingColumn).toBe(0);
+    for (let i = 0; i < 100; i++) {
+      const shipPosition = generateRandomPosition({ name: 'cruiser', size: 10 });
+      expect(shipPosition.startingColumn).toBe(0);
+    }
   });
 
   test('should generate random positions for the same ship size', () => {
