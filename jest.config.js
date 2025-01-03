b4c5cf802a56e98473809afc0ddab004c1a2a6d7
Ceri <ceriwoolway@gmail.com>
2025-01-03 19:56:51 +0000

    Test coverage addition (#11)
    * Test coverage addition
    
    * Update workflow files
    
    * Update permissions
    
    * Further coverage updates
    
    * More tweaks
    
    * More tweaks
    
    * Update Jest config
    
    * Swap to codecov.io
    
    * Try lcov instead

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
   },
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts', '!src/index.tsx', '!src/reportWebVitals.ts'],
+  coverageReporters: ['json-summary', 'text', 'lcov'],
 };
