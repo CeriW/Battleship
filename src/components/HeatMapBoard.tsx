af0325e344036ab2c885bbde9c5ec210ef3f6ff3
Ceri <ceriwoolway@gmail.com>
2025-02-07 15:35:48 +0000

    Highlight max value on heat map board

--- src/components/HeatMapBoard.tsx
+++ src/components/HeatMapBoard.tsx
@@ -14,30 +14,42 @@ export const HeatMapBoard = ({ positions }: any) => {
   const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
   const rows = [];
 
-  for (let i = 0; i < letters.length; i++) {
+  // Find the highest value in the positions array
+  let maxValue = 0;
+  for (let y = 0; y < 10; y++) {
+    for (let x = 0; x < 10; x++) {
+      maxValue = Math.max(maxValue, positions[y][x]);
+    }
+  }
+
+  for (let y = 0; y < letters.length; y++) {
     const cells = [];
-    for (let j = 0; j < 10; j++) {
-      const percentageDecimal = positions[i][j] / ai.heatMapSimulations;
+    for (let x = 0; x < 10; x++) {
+      const percentageDecimal = positions[y][x] / ai.heatMapSimulations;
       const percentage = (percentageDecimal * 100).toFixed(1);
 
       cells.push(
         <div
-          key={`cell-${letters[i]}-${j}`}
+          key={`cell-${letters[y]}-${x}`}
           className="cell"
-          style={{ backgroundColor: `rgba(255, 0, 0, ${percentageDecimal})` }}
+          style={{
+            // TODO - Not sure if I'm sticking with this. Should move to CSS if I do.
+            backgroundColor: positions[y][x] === maxValue ? 'black' : `rgba(255, 0, 0, ${percentageDecimal})`,
+            color: positions[y][x] === maxValue ? 'white' : `black`,
+          }}
           data-testid="cell"
         >
-          {positions[i][j] > 0 && positions[i][j] < ai.heatMapSimulations ? percentage + '%' : ''}
-          {positions[i][j] === 0 ? '❌' : ''}
-          {positions[i][j] === ai.heatMapSimulations ? '✔️' : ''}
+          {positions[y][x] > 0 && positions[y][x] < ai.heatMapSimulations ? percentage + '%' : ''}
+          {positions[y][x] === 0 ? '❌' : ''}
+          {positions[y][x] === ai.heatMapSimulations ? '✔️' : ''}
         </div>
       );
     }
 
     rows.push(
-      <div className="row" key={`row-${i}`}>
-        <div className="row-marker" key={`row-marker-${i}`} data-testid="row-marker">
-          {letters[i]}
+      <div className="row" key={`row-${y}`}>
+        <div className="row-marker" key={`row-marker-${y}`} data-testid="row-marker">
+          {letters[y]}
         </div>
         {cells}
       </div>
