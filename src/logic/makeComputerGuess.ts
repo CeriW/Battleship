e557be625faa7c441e47ce0bc4ac3bed25375da5
Ceri <ceriwoolway@gmail.com>
2025-02-28 15:38:52 +0000

    Computer can mark ship sunk on its own guesses

--- src/logic/makeComputerGuess.ts
+++ src/logic/makeComputerGuess.ts
@@ -27,15 +27,31 @@ export const useMakeComputerGuess = () => {
     const x = maxValueIndex % 10;
 
     const cell = userShips[y][x];
+    // If this is an unguessed or empty cell
     if (cell?.status === CellStates.unguessed || !cell) {
       const newUserShips = [...userShips.map((row) => [...row])];
+
+      let sunk = false;
+      const status = cell?.name ? CellStates.hit : CellStates.miss;
+
       newUserShips[y][x] = {
         name: cell?.name || null,
-        status: cell?.name ? CellStates.hit : CellStates.miss,
-        sunk: Boolean(cell?.name),
+        status,
+        sunk,
       };
+
+      if (cell?.name) {
+        // If there is a ship here, check whether it is now fully sunk
+        const shipCells = newUserShips.flat().filter((ship) => ship?.name === cell.name);
+        sunk = shipCells.every((ship) => ship?.status === CellStates.hit);
+
+        if (sunk) {
+          addToLog(`Computer sunk ${cell.name}`);
+        }
+      }
+
       setUserShips(newUserShips);
-      addToLog(`Computer guessed ${letters[y]}${x + 1}, ${cell?.name ? 'hit' : 'miss'}`);
+      addToLog(`Computer guessed ${letters[y]}${x + 1}, ${status}`);
     }
   }, [userShips, setUserShips, addToLog]);
 };
