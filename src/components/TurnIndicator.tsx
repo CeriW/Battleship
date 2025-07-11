5711d30c7df8be4629bdd9e3e32428b7301882b7
Ceri <ceriwoolway@gmail.com>
2025-07-11 13:58:29 +0100

    Implement styling

--- src/components/TurnIndicator.tsx
+++ src/components/TurnIndicator.tsx
@@ -2,10 +2,5 @@ import React, { useContext } from 'react';
 import { GameContext } from '../GameContext';
 
 export const TurnIndicator = ({ playerTurn }: { playerTurn: 'user' | 'computer' }) => {
-  return (
-    <div className="turn-indicator">
-      <h3>Turn Indicator</h3>
-      <p>Current turn: {playerTurn}</p>
-    </div>
-  );
+  return <div className="turn-indicator">{playerTurn === 'user' ? 'Your turn' : 'Computer turn'}</div>;
 };
