22f902b525cb5ab959d32e480f42f4cbe718fc04
Ceri <ceriwoolway@gmail.com>
2025-07-11 14:11:05 +0100

    Update image files

--- src/components/TurnIndicator.tsx
+++ src/components/TurnIndicator.tsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { GameContext } from '../GameContext';
 
 export const TurnIndicator = ({ playerTurn }: { playerTurn: 'user' | 'computer' }) => {
   return <div className="turn-indicator">{playerTurn === 'user' ? 'Your turn' : 'Computer turn'}</div>;
