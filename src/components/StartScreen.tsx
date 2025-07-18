641c731f5dc616f256b0ae633e7bc2ef622c1e88
Ceri <ceriwoolway@gmail.com>
2025-07-18 14:08:47 +0100

    Sets AI level when clicked

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import gameTitle from '../img/game-title.png';
 import emilyHappyPng from '../img/avatars/emily/happy.png';
 import AlexHappyPng from '../img/avatars/alex/happy.png';
 import NatashaHappyPng from '../img/avatars/natasha/happy.png';
 
+import { GameContext } from '../GameContext';
+
 export const StartScreen = () => {
+  const { setAiLevel } = useContext(GameContext);
   return (
     <div className="start-screen">
       <img src={gameTitle} alt="Battleship" className="game-title" />
 
       <h2>Choose your opponent</h2>
       <div className="difficulty-selector">
-        <div className="difficulty-selector-item emily">
+        <div className="difficulty-selector-item emily" onClick={() => setAiLevel('easy')}>
           <img src={emilyHappyPng} alt="Emily" />
           <div className="difficulty-selector-item-name">
             <div className="difficulty-selector-name">Emily</div>
@@ -19,7 +22,7 @@ export const StartScreen = () => {
           </div>
         </div>
 
-        <div className="difficulty-selector-item alex">
+        <div className="difficulty-selector-item alex" onClick={() => setAiLevel('medium')}>
           <img src={AlexHappyPng} alt="Alex" />
           <div className="difficulty-selector-item-name">
             <div className="difficulty-selector-name">Alex</div>
@@ -27,7 +30,7 @@ export const StartScreen = () => {
           </div>
         </div>
 
-        <div className="difficulty-selector-item natasha">
+        <div className="difficulty-selector-item natasha" onClick={() => setAiLevel('hard')}>
           <img src={NatashaHappyPng} alt="Natasha" />
           <div className="difficulty-selector-item-name">
             <div className="difficulty-selector-name">Natasha</div>
